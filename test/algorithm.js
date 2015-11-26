66b42688bccbc8946e51495c8d1775cdc01e7b90
Xinyu <xyzhang.me@aliyun.com>
2015-11-25 21:59:15 -0800

    get test points data

---
+++ test/algorithm.js
@@ -0,0 +1,17 @@
+var algorithm = require('../');
+
+// describe('Basic Test', function() {
+//     describe('#algorithm()', function () {
+//         it('should be called without exception.', function (done) {
+     	    
+//         });
+
+//         it('should return a score.', function (done) {
+     	    
+//         });
+//     });
+// });
+
+algorithm.FrameRate(5);
+
+
